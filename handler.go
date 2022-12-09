6dfdc3812d42143972e5e7b47d14ff7184d22cf6
Wojciech Kaczmarek <wojtekk@kofeina.net>
2022-12-09 01:55:45 +0100

    text/plain nosniff for the normal response

--- handler.go
+++ handler.go
@@ -49,6 +49,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.WriteHeader(201)
 	fmt.Fprintln(w, "CREATED", destPath)
 }
